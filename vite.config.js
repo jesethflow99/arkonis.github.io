c7d9d4c168a2d0be6930a41e635f5cfee5a36c71
Jeseth <ceggarr199@gmail.com>
2026-07-02 23:38:29 -0600

    fix base path for project page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/arkonis.github.io/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
